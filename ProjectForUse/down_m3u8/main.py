522faafe8d3e7bf6e2d25f5c2b915d577afa3fbc
yyl <Ocean_yyl@163.com>
2020-01-17 11:19:47 +0800

    消除安全警告

--- ProjectForUse/down_m3u8/main.py
+++ ProjectForUse/down_m3u8/main.py
@@ -6,7 +6,8 @@ from package.combine_ts import combine
 from package.down_ts import download, get_ts_urls
 
 # m3u8_url文件地址
-m3u8_url = 'https://jdvodrvfb210d.vod.126.net/mooc-video/nos/hls/2018/04/28/1009218006_cff89340b62041e396ec9a91a9974a81_sd.m3u8'
+# m3u8_url = 'https://jdvodrvfb210d.vod.126.net/mooc-video/nos/hls/2018/04/28/1009218006_cff89340b62041e396ec9a91a9974a81_sd.m3u8'
+m3u8_url = 'blob:https://hywm1.com/6145f824-3723-45a3-ad6d-fb1297b45824'
 # 要保存的文件名
 video_name = "result/h2.ts"
 
@@ -14,7 +15,7 @@ video_name = "result/h2.ts"
 def down_m3u8(m3u8_url,video_name):
     ts_urls = get_ts_urls(m3u8_url)
     m3u8_file_name = m3u8_url.split("/")[-1]
-    temp_download_path = "ts_list_temp"
+    temp_download_path = m3u8_file_name+"ts_list_temp"
     # 创建目录ts_list_temp
     if not os.path.exists(temp_download_path):
         os.mkdir(temp_download_path)
--- tools/log/log_to_file.py
+++ tools/log/log_to_file.py
@@ -13,32 +13,3 @@ logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
 if __name__ == '__main__':
     logging.info("hello loginfo")
 
-"""
-日志级别
- 
-日志一共分成5个等级，从低到高分别是：DEBUG INFO WARNING ERROR CRITICAL。
-DEBUG：详细的信息,通常只出现在诊断问题上
-INFO：确认一切按预期运行
-WARNING：一个迹象表明,一些意想不到的事情发生了,或表明一些问题在不久的将来(例如。磁盘空间低”)。这个软件还能按预期工作。
-ERROR：更严重的问题,软件没能执行一些功能
-CRITICAL：一个严重的错误,这表明程序本身可能无法继续运行
-这5个等级，也分别对应5种打日志的方法： debug 、info 、warning 、error 、critical。默认的是WARNING，当在WARNING或之上时才被跟踪。
-日志格式说明
- 
-logging.basicConfig函数中，可以指定日志的输出格式format，这个参数可以输出很多有用的信息，如上例所示：
-%(levelno)s: 打印日志级别的数值
-%(levelname)s: 打印日志级别名称
-%(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
-%(filename)s: 打印当前执行程序名
-%(funcName)s: 打印日志的当前函数
-%(lineno)d: 打印日志的当前行号
-%(asctime)s: 打印日志的时间
-%(thread)d: 打印线程ID
-%(threadName)s: 打印线程名称
-%(process)d: 打印进程ID
-%(message)s: 打印日志信息
-我在工作中给的常用格式在前面已经看到了。就是：
-format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'
-这个格式可以输出日志的打印时间，是哪个模块输出的，输出的日志级别是什么，以及输入的日志
-
-"""
